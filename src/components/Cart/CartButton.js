cfe419bfa3c9bb44f59b5e29c1f6833dbec28412
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-04-21 10:53:07 +0300

    show/hide cart logic implemented

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,19 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
+
+
 
 const CartButton = (props) => {
+
+  const dispatch = useDispatch();
+
+  const toggleCartHandle = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandle}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
